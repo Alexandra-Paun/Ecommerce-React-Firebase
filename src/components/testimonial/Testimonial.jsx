5997b3f9c2b3164376fa1412d0ab205db828f15a
Alexandra Paun <alexandrapaun96@yahoo.com>
2024-03-13 05:29:03 +0200

    Update testimonials

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -14,28 +14,28 @@ function Testimonial() {
                         <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
                             <div className="h-full text-center">
                                 <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://ecommerce-sk.vercel.app/img/kamal.png" />
-                                <p style={{color: mode === 'dark' ? 'white' : ''}} className="leading-relaxed">PARAGRAF</p>
+                                <p style={{color: mode === 'dark' ? 'white' : ''}} className="leading-relaxed">"A flawless reputation is evidence of successful partnerships. We value what our customers think about us, and we’re sure you do too."</p>
                                 <span className="inline-block h-1 w-10 rounded bg-blue-500 mt-6 mb-4" />
-                                <h2 style={{color: mode === 'dark' ? '#ff4162' : ''}} className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">Kamal Nayan Upadhyay</h2>
+                                <h2 style={{color: mode === 'dark' ? '#ff4162' : ''}} className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">Tim Trent</h2>
                                 <p style={{color: mode === 'dark' ? 'white' : ''}} className="text-gray-500">Senior Product Designer</p>
                             </div>
                         </div>
                         <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
                             <div className="h-full text-center">
                                 <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://cdn-icons-png.flaticon.com/128/2763/2763444.png" />
-                                <p  style={{color: mode === 'dark' ? 'white' : ''}}className="leading-relaxed">PARARGRAF</p>
+                                <p  style={{color: mode === 'dark' ? 'white' : ''}}className="leading-relaxed">"I am very glad with the service. Keep up the good work!"</p>
                                 <span className="inline-block h-1 w-10 rounded bg-blue-500 mt-6 mb-4" />
                                 <h2 style={{color: mode === 'dark' ? '#ff4162' : ''}} className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">React Js</h2>
-                                <p style={{color: mode === 'dark' ? 'white' : ''}} className="text-gray-500">UI Develeoper</p>
+                                <p style={{color: mode === 'dark' ? 'white' : ''}} className="text-gray-500">John Nance</p>
                             </div>
                         </div>
                         <div className="lg:w-1/3 lg:mb-0 p-4">
                             <div className="h-full text-center">
                                 <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://webknudocs.vercel.app/logo/react.png" />
-                                <p style={{color: mode === 'dark' ? 'white' : ''}} className="leading-relaxed">PARAGRAF</p>
+                                <p style={{color: mode === 'dark' ? 'white' : ''}} className="leading-relaxed">"I love your services"</p>
                                 <span className="inline-block h-1 w-10 rounded bg-blue-500 mt-6 mb-4" />
-                                <h2 style={{color: mode === 'dark' ? '#ff4162' : ''}} className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">React Js</h2>
-                                <p  style={{color: mode === 'dark' ? 'white' : ''}}className="text-gray-500">CTO</p>
+                                <h2 style={{color: mode === 'dark' ? '#ff4162' : ''}} className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">General Manager</h2>
+                                <p  style={{color: mode === 'dark' ? 'white' : ''}}className="text-gray-500">Gerline Isidora</p>
                             </div>
                         </div>
                     </div>
