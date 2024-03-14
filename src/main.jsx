1118ce5b404be6a8488797db4451a1d1392209e1
Alexandra Paun <alexandrapaun96@yahoo.com>
2024-03-14 08:23:24 +0200

    Update deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {store} from './redux/store.jsx'
 import { Provider } from 'react-redux';
-
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,6 +10,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
     <App />
     </Provider>
-
   </React.StrictMode>,
 )
